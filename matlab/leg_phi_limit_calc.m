cce85573bf1ac64066db3de9a33032fa547b02d1
小企鹅 <1357482552@qq.com>
2023-11-06 20:06:14 +0800

    ✨ feat(五连杆解算): 添加了角度范围限制
    根据腿长计算摆角phi_0的范围

---
+++ matlab/leg_phi_limit_calc.m
@@ -0,0 +1,19 @@
+% 本程序用于计算腿部角度限幅大小，保证关节解算有实数解
+
+clear;
+clc;
+% 设定所需符号
+syms l0;
+l1=0.095;l2=0.19;l3=l2;l4=l1;l5=0.095; % 腿部杆长(单位：m)
+
+%求解下限
+min = acos(((l1+l2)^2-l0^2-(l5/2)^2)/(l0*l5));
+%求解上限
+max = acos(((l3+l4)^2-l0^2-(l5/2)^2)/(-l0*l5));
+
+% 求得腿部姿态 [min; max] = PhiLimit(l0)
+limit=[min; max];
+matlabFunction(limit,'File','PhiLimit');
+
+
+disp("角度限幅函数生成完毕");
--- matlab/picture.m
+++ matlab/picture.m
@@ -1,22 +1,32 @@
 
 clear
 % 定义 l0 和 phi0 的范围
-l0_range = linspace(0.14, 0.2, 100); % 0 到 10 之间取 100 个点
-phi0_range = linspace(pi/6, pi*5/6, 100); % 0 到 2*pi 之间取 100 个点
+l0_range = linspace(0.13, 0.24, 50); % 0 到 10 之间取 100 个点
+phi0_range = linspace(0.61, 2.40, 50); % 0 到 2*pi 之间取 100 个点
 
 % 生成 l0 和 phi0 的网格
 [l0_grid, phi0_grid] = meshgrid(l0_range, phi0_range);
 
 % 计算对应的函数值
 z  = JointPos(l0_grid, phi0_grid);
-z = z(101:200, 1:100);
 
 % 将复数值置为 0
 z(imag(z) ~= 0) = 0;
+z1 = z(1:50, 1:50);
+z2 = z(51:100, 1:50);
 
 % 绘制三维图像
-surf(l0_grid, phi0_grid, z);
+figure;% 创建一个新的图形窗口
+surf(l0_grid, phi0_grid, z1);
 xlabel('l0');
 ylabel('phi0');
-zlabel('f(l0, phi0)');
-title('Graph of the Expression');
+zlabel('关节设定角度');
+title('关节1的设定角度和l0,phi0的关系');
+
+
+figure;% 创建一个新的图形窗口
+surf(l0_grid, phi0_grid, z2);
+xlabel('l0');
+ylabel('phi0');
+zlabel('关节设定角度');
+title('关节2的设定角度和l0,phi0的关系');
