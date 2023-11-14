b4695d8a3070c74cc1437b760845d4405407af52
小企鹅 <1357482552@qq.com>
2023-11-15 00:51:29 +0800

    添加KF

--- stm32_code/application/EKF.c
+++ stm32_code/application/EKF.c
@@ -34,6 +34,25 @@ void InitEKF(EKF_s *ekf, float Q, float R)
 }
 
 /**
+ *@brief       卡尔曼滤波器
+ *@param[in]   KFP 卡尔曼结构体参数
+ *@param[in]   input 需要滤波的参数的测量值（即传感器的采集值）
+ *@return 滤波后的参数（最优值）
+ */
+float KalmanFilterCalc(KF_s *kfp, float input)
+{
+    // 预测协方差方程：k时刻系统估算协方差 = k-1时刻的系统协方差 + 过程噪声协方差
+    kfp->Now_P = kfp->LastP + kfp->Q;
+    // 卡尔曼增益方程：卡尔曼增益 = k时刻系统估算协方差 / （k时刻系统估算协方差 + 观测噪声协方差）
+    kfp->Kg = kfp->Now_P / (kfp->Now_P + kfp->R);
+    // 更新最优值方程：k时刻状态变量的最优值 = 状态变量的预测值 + 卡尔曼增益 * （测量值 - 状态变量的预测值）
+    kfp->out = kfp->out + kfp->Kg * (input - kfp->out); // 因为这一次的预测值就是上一次的输出值
+    // 更新协方差方程: 本次的系统协方差付给 kfp->LastP 威下一次运算准备。
+    kfp->LastP = (1 - kfp->Kg) * kfp->Now_P;
+    return kfp->out;
+}
+
+/**
  * @brief      扩展卡尔曼滤波器
  * @param[in]  ekf *ekf 卡尔曼结构体参数
  * @param[in]  float input1 需要滤波的参数1的测量值（即传感器的采集值）
--- stm32_code/application/EKF.h
+++ stm32_code/application/EKF.h
@@ -9,7 +9,19 @@ typedef struct
     float R;     // 观测噪声协方差
 } EKF_s;         // Kalman Filter parameter
 
+typedef struct
+{
+    float LastP; // 上次估算协方差
+    float Now_P; // 当前估算协方差
+    float out;   // 卡尔曼滤波器输出
+    float Kg;    // 卡尔曼增益
+    float Q;     // 过程噪声协方差
+    float R;     // 观测噪声协方差
+} KF_s;          // Kalman Filter parameter
+
 void InitEKF(EKF_s *ekf, float Q, float R);
+
+float KalmanFilterCalc(KF_s *kfp, float input);
 
 float ExtendedKalmanFilterCalc(EKF_s *ekf, float input1, float input2);
 
